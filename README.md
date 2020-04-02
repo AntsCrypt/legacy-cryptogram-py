13d71555ad38164c0c080bee1a60854123ec4c29
RodrigoDornelles <rodrigo@dornelles.me>
2020-04-02 11:38:41 -0300

    database generation improvements and optimizations

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ### como gerar banco de dados: ###
 ```SHELL
-python3 generatory.py > output.json
+time python3 generatory.py > output.json < words.txt
 ```
--- generator.py
+++ generator.py
@@ -1,36 +1,73 @@
+from collections import defaultdict
 from slugify import slugify
 import json
+import sys
 import re
 
-generator = {}
+# Creates instance of defaultdict
+def tree(): 
+    return defaultdict(tree)
 
-def vogais(string):
+# Add Word
+def add(t, path):
+    for node in path:
+        t = t[node]
+
+# Add value to tree
+def add(t, patch, value):
+    i = 0
+    for node in patch:
+        i += 1
+        if i == len(patch): 
+            if not isinstance(t[node], list):
+                t[node] = [value]
+            elif not value in t[node]:
+                t[node].append(value)
+        t = t[node]
+
+# Count number of vowels
+def vowels(string):
     return len(re.sub('[^aeiou]', '', string))
 
+# Loads database
 def run_generator():
-    with open('palavras.txt') as lista_palavras:
-        for palavra in lista_palavras:
-            
-            palavra = slugify(palavra)
-
-            if palavra.find('-') >= 0:
-                continue
-
-            len_vogais = str(vogais(palavra))
-            len_palavra = str(len(palavra))
-
-            if not len_palavra in generator:
-                generator[len_palavra] = {}
-
-            if not len_vogais in generator[len_palavra]:
-                generator[len_palavra][len_vogais] = []
-
-            if palavra in generator[len_palavra][len_vogais]:
-                continue
-
-            generator[len_palavra][len_vogais].append(palavra)
+    while True:
+        # Scan word
+        word = slugify(sys.stdin.readline())
         
+        # Words over
+        if len(word) == 0:
+            break
 
+        # Ignore multiple words in line
+        if word.find('-') >= 0:
+            continue
+
+        # Count characters
+        count_char = {}
+        len_char = []
+        for char in word:
+            count_char[char] = word.count(char)
+
+        # Key char
+        for char in count_char.keys():
+            len_char.append(char  + str(count_char[char]))
+        
+        # Key count
+        len_vowel = str(vowels(word))
+        len_word = str(len(word))
+        
+        # All keys
+        tree_key = [len_word, len_vowel]
+        tree_key.extend(sorted(len_char))
+
+        # Add Word
+        add(generator, tree_key, word)
+
+# Database
+generator = tree()
+        
+# Runs command
 if __name__ == '__main__':
     run_generator()
-    print(json.dumps(generator, indent=2))
+    sys.stdout.write(json.dumps(generator))
